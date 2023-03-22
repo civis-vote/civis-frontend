f3d3235d093423c7204d7feac08cd3b83ab16d02
Saurabh Tiwari <tiwarisaurabhsde@gmail.com>
2023-03-22 11:07:18 +0530

    Feat(CIV-704): About us page revamp

--- src/app/modules/about-us/about-us.component.ts
+++ src/app/modules/about-us/about-us.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: [
+    "./css/normalize.css",
+    "./css/webflow.css",
+    "./css/civis-about-page.webflow.css",
+  ],
 })
-
 export class AboutUsComponent implements OnInit {
+  openVideoModal: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  playVideo() {
+    this.openVideoModal = true;
+  }
+
+  closeVideo() {
+    this.openVideoModal = false;
+  }
 }

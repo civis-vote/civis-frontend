512fc620f82b04485290ad1dfaaf53b6548012e7
B G NIKHIL <nikhilgowda69702@gmail.com>
2024-09-19 12:44:55 +0530

    refactor: code review changes, removed negative margin for dividers.

--- src/app/shared/services/token.service.ts
+++ src/app/shared/services/token.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TokenService {
   token: string;
@@ -15,13 +15,16 @@ export class TokenService {
 
   tokenHandler() {
     this.checkTokenExpiration();
-    if (localStorage.getItem('civis-token') && localStorage.getItem("civis-token_expires")) {
-      this.token = localStorage.getItem('civis-token');
+    if (
+      localStorage.getItem("civis-token") &&
+      localStorage.getItem("civis-token_expires")
+    ) {
+      this.token = localStorage.getItem("civis-token");
       this.expiresAt = localStorage.getItem("civis-token_expires");
       this.hasToken$.next(true);
     } else {
-      this.token = '';
-      this.expiresAt = '';
+      this.token = "";
+      this.expiresAt = "";
       this.hasToken$.next(false);
       localStorage.removeItem("civis-token");
       localStorage.removeItem("civis-token_expires");
@@ -34,26 +37,33 @@ export class TokenService {
 
     const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{1,3})?Z$/;
 
-    if (typeof tokenExpirationString !== 'string' || !isoDateRegex.test(tokenExpirationString)) {
-        localStorage.removeItem("civis-token");
-        localStorage.removeItem("civis-token_expires");
-        return;
+    const clearTokenData = () => {
+      localStorage.removeItem("civis-token");
+      localStorage.removeItem("civis-token_expires");
+    };
+
+    if (
+      typeof tokenExpirationString !== "string" ||
+      !isoDateRegex.test(tokenExpirationString)
+    ) {
+      clearTokenData();
+      return;
     }
 
     const tokenExpirationTime = new Date(tokenExpirationString);
 
-    if (isNaN(tokenExpirationTime.getTime()) || (currentTime > tokenExpirationTime.toISOString())) {
-        localStorage.removeItem("civis-token");
-        localStorage.removeItem("civis-token_expires");
+    if (
+      isNaN(tokenExpirationTime.getTime()) ||
+      currentTime > tokenExpirationTime.toISOString()
+    ) {
+      clearTokenData();
     }
-}
-
-
+  }
 
   storeToken(tokenObject: any) {
     const token = tokenObject.accessToken;
     const expiresAt = tokenObject.expiresAt;
-    localStorage.setItem('civis-token', token);
+    localStorage.setItem("civis-token", token);
     localStorage.setItem("civis-token_expires", expiresAt);
   }
 }

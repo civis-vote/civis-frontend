aecab8ca096276cd212f867e967897119ed7e681
B G NIKHIL <105233916+Bandinikhil@users.noreply.github.com>
2024-09-23 18:10:42 +0530

    PRO-5944-2 (#486)
    * feat: Added Organisation details and response counts in the Profile card (#462)
    
    * feat: Added Organisation details and response counts in the Profile card
    
    * refactor: code review changes
    
    * fix: feedback points changes, 500 error debugging.
    
    * refactor: checkTokenExpiration function, & code review changes
    
    * refactor: code review changes
    
    * refactor: code review changes, removed negative margin for dividers.
    
    * refactor: code review changes
    
    * refactor: Renamed class name to divider
    
    * refactor: used Shorthand property for styling
    
    * Authentication successfull, loading Civis Platform...
    
    * refactor: code review changes

--- src/app/shared/services/token.service.ts
+++ src/app/shared/services/token.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TokenService {
   token: string;
@@ -15,13 +15,16 @@ export class TokenService {
 
   tokenHandler() {
     this.checkTokenExpiration();
-    if (localStorage.getItem('civis-token') && localStorage.getItem("civis-token_expires")) {
-      this.token = localStorage.getItem('civis-token');
+    if (
+      localStorage.getItem("civis-token") &&
+      localStorage.getItem("civis-token_expires")
+    ) {
+      this.token = localStorage.getItem("civis-token");
       this.expiresAt = localStorage.getItem("civis-token_expires");
       this.hasToken$.next(true);
     } else {
-      this.token = '';
-      this.expiresAt = '';
+      this.token = "";
+      this.expiresAt = "";
       this.hasToken$.next(false);
       localStorage.removeItem("civis-token");
       localStorage.removeItem("civis-token_expires");
@@ -31,19 +34,18 @@ export class TokenService {
   checkTokenExpiration() {
     const currentTime = new Date().toISOString();
     const tokenExpirationTime = localStorage.getItem("civis-token_expires") ?? new Date(0).toISOString();
-   
+
     // logout user if token expired
     if (currentTime > tokenExpirationTime) {
       localStorage.removeItem("civis-token");
       localStorage.removeItem("civis-token_expires");
-    } 
+    }
   }
-
 
   storeToken(tokenObject: any) {
     const token = tokenObject.accessToken;
     const expiresAt = tokenObject.expiresAt;
-    localStorage.setItem('civis-token', token);
+    localStorage.setItem("civis-token", token);
     localStorage.setItem("civis-token_expires", expiresAt);
   }
 }

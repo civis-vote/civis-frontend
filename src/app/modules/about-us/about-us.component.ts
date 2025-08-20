83803c85df02504e008e7357b8bbfb6018f454d7
ktm-barath <barath@commutatus.com>
2025-08-20 11:00:41 +0530

    refactor: Removed temporary changes and Logo updates (#637)
    * refactor: Update team members, remove Vikasit Maharashtra banner and update office address
    
    * feat: Update partner logos and implement scrolling carousel for government partners
    
    * fix: Minor change
    
    * fix: Minor change
    
    * fix: Feedbacks

--- src/app/modules/about-us/about-us.component.ts
+++ src/app/modules/about-us/about-us.component.ts
@@ -38,81 +38,122 @@ export class AboutUsComponent {
 
   teamMembers = [
     {
-      name: "Antaraa Vasudev",
-      image: "assets/images/about-us/Antaraa Vasudev.jpeg",
-      description: "Founder & CEO",
-      href: "https://www.linkedin.com/in/antaraavasudev/",
-      class: "first",
-    },
-    {
       name: "Akalya V",
       image: "assets/images/about-us/Akalya V.jpeg",
       description: "Associate, Communications",
       href: "https://www.linkedin.com/in/akalya-veerappan-b9b346203",
-      class: "next long-description",
+      class: "long-description",
+    },
+    {
+      image: "assets/images/team/Alaksha-Dhakite.jpg",
+      name: "Alaksha Dhakite",
+      description: "Associate, Partnerships",
+      href: "https://www.linkedin.com/in/alaksha-dhakite-19822b164/",
+      class: "long-description",
+    },
+    {
+      name: "Antaraa Vasudev",
+      image: "assets/images/about-us/Antaraa Vasudev.jpeg",
+      description: "Founder & CEO",
+      href: "https://www.linkedin.com/in/antaraavasudev/",
+      class: "long-description",
     },
     {
       name: "Atharva Joshi",
       image: "assets/images/about-us/Atharva Joshi.jpeg",
       description: "Senior Product Manager",
       href: "https://www.linkedin.com/in/atharva-joshi-b80356153",
-      class: "atharva",
+      class: "long-description",
     },
     {
-      name: "Brishti Mondal",
-      image: "assets/images/about-us/Brishti Mondal.jpeg",
-      description: "Consultation Fellow",
-      href: "https://www.linkedin.com/in/brishtimondal",
-      class: "next long-description",
+      name: "Gargi Surana",
+      image: "assets/images/team/Gargi-Surana.jpg",
+      description: "Associate, Governance",
+      href: "https://www.linkedin.com/in/gargisurana/",
+      class: "long-description",
     },
     {
       name: "Hetvi Chheda",
       image: "assets/images/about-us/Hetvi Chheda.jpeg",
       description: "Senior Associate, Governance",
       href: "https://www.linkedin.com/in/hetvi-chheda-ab50b1192",
-      class: "next",
+      class: "long-description",
     },
     {
-      name: "Mallika Dandekar",
-      image: "assets/images/about-us/Mallika Dandekar.jpeg",
-      description: "Associate, Governance",
-      href: "https://www.linkedin.com/in/mallikadandekar/",
-      class: "next long-description",
+      name: "Sukirat Kaur",
+      image: "assets/images/team/Sukirat-Kaur.jpg",
+      description: "Consultation Fellow",
+      href: "https://www.linkedin.com/in/sukirat-kaur-6a816820b/",
+      class: "long-description",
+    },
+    {
+      name: "Swetha A N",
+      image: "assets/images/team/Swetha-A-N.jpg",
+      description: "Administrative Officer",
+      href: "https://www.linkedin.com/in/swetha-anantha-narayanan-9511871b4/",
+      class: "long-description",
     },
     {
       name: "Vagda Galhotra",
       image: "assets/images/about-us/Vagda Galhotra.jpeg",
       description: "Senior Associate, Outreach and Comms",
       href: "https://www.linkedin.com/in/vagda-galhotra-320a04269",
-      class: "next long-description",
-    }
+      class: "long-description",
+    },
   ];
 
   pastPartners = [
     {
-      image: "assets/images/about-us/NIUA_Logo_new 1 1.png",
+      image: "assets/images/about-us/government-partners/1. Government of Maharashtra.jpg",
+      alt: "Government of Maharashtra",
+    },
+    {
+      image: "assets/images/about-us/government-partners/2. BMC.jpg",
+      alt: "BMC",
+    },
+    {
+      image: "assets/images/about-us/government-partners/3. OCAC.jpg",
+      alt: "OCAC",
+    },
+    {
+      image: "assets/images/about-us/government-partners/4. Karmayogi Bharat.webp",
+      alt: "Karmayogi Bharat",
+    },
+    {
+      image: "assets/images/about-us/government-partners/5. Manipur State Rural Livelihoods Mission.png",
+      alt: "Manipur State Rural Livelihoods Mission",
+    },
+    {
+      image: "assets/images/about-us/government-partners/6. Securities and Exchange Board of India.png",
+      alt: "Securities and Exchange Board of India",
+    },
+    {
+      image: "assets/images/about-us/government-partners/7. Maharashtra State Innovaiton Society.png",
+      alt: "Maharashtra State Innovation Society",
+    },
+    {
+      image: "assets/images/about-us/government-partners/8. Central Board of Secondary Education.png",
+      alt: "Central Board of Secondary Education",
+    },
+    {
+      image: "assets/images/about-us/government-partners/9. National Institute of Urban Affairs.png",
       alt: "National Institute of Urban Affairs",
-      href: "https://medium.com/civis-vote/engaging-communities-in-the-future-of-indias-cities-7b795ecb99a5",
     },
     {
-      image: "assets/images/about-us/logo (1) 1 1.png",
-      alt: "CBSE",
-      href: "https://medium.com/civis-vote/enabling-educators-learnings-from-a-consultation-10b084fafba4",
+      image: "assets/images/about-us/government-partners/10. Supreme Court E-committee.png",
+      alt: "Supreme Court E-committee",
     },
     {
-      image: "assets/images/about-us/MSInS_Logo 1 1.png",
-      alt: "MSInS",
-      href: "https://twitter.com/MSInSociety/status/1656238177018064896",
+      image: "assets/images/about-us/government-partners/11. NITI Aayog.jpg",
+      alt: "NITI Aayog",
     },
     {
-      image: "assets/images/about-us/gmda_logo_trans 1 1.png",
+      image: "assets/images/about-us/government-partners/12. GMDA.webp",
       alt: "GMDA",
-      href: "https://medium.com/civis-vote/building-dialogue-a-new-beginning-for-citizens-engagement-6caff57672be",
     },
     {
-      image: "assets/images/about-us/NITI_Aayog_logo 1 1.png",
-      alt: "NITI AAYOG",
-      href: "https://medium.com/civis-vote/justice-delivery-goes-digital-826938401e1",
+      image: "assets/images/about-us/government-partners/13. IChangeMyCity.png",
+      alt: "IChangeMyCity",
     },
   ];
 
